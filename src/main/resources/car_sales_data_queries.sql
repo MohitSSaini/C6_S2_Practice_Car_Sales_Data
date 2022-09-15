424a0588f25cc2aff606f00def3dd794c45cb4a5
Mohit Saini <mohit.saini09868@gmail.com>
2022-09-15 12:59:54 +0530

    create the data car_sales_data_queries.sql in resource file

---
+++ src/main/resources/car_sales_data_queries.sql
@@ -0,0 +1,42 @@
+-- create the 'CarSales' database
+
+-- use the 'CarSales' database
+
+-- create the 'SalesPerson' table with the following columns:
+-- 'SalesID'
+-- 'FirstName'
+-- 'City'
+-- 'CommissionRate'
+
+-- insert the following records into the 'SalesPerson' table:
+-- 1001, John, London, 12
+-- 1002, Bob, San Jose, 13
+-- 1004, Judith, London, 11
+-- 1007, Robin, Barcelona, 15
+-- 1003, Harry, New York, 10
+-- 1005, James, London, 26
+-- 1008, Charles, Florida, 0
+
+-- fetch all the details of the sales persons
+
+-- retrieve the salesperson with the first name Robin and city Barcelona.
+
+-- get the city and commission rate of John
+
+-- Fetch all the details of salespersons with a commission rate greater than 15
+
+-- Fetch all the details of salespersons with a commission rate between 10 and 20.
+
+-- Retrieve the first name and city of salespersons who have a commission rate of more than 25.
+
+-- Get the first name and city of salespersons who have a commission rate as 0.
+
+-- Fetch all the salespersons whose name begins with ‘J’.
+
+-- Retrieve all the salespersons from London, New York, and San Jose.
+
+-- Find the average commission rate of a salesperson.
+
+-- Retrieve the names of all the salespersons with a commission rate of 10, 11, 12, and 13.
+
+-- Find the salesperson and city that has the maximum commission rate.
